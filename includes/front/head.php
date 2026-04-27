fe12025e3156ee51b83406d6ae6ac1e9b850cebf
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2026-04-27 21:22:44 +0530

    header and footer template

--- includes/front/head.php
+++ includes/front/head.php
@@ -1,9 +1,10 @@
 <?php
 
-function p_head(){
+function p_head()
+{
     ?>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <?php
 }
 ?>
